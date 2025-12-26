6081e3fa124d322847f6f05e0b45b1d2e136396c
Cássio Cintra <cassiocr2012@hotmail.com>
2025-12-26 10:06:46 -0300

    add project structure

---
+++ src/main/java/com/cassio/polychat/PolymorphicChatApplication.java
@@ -0,0 +1,13 @@
+package com.cassio.polychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolymorphicChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolymorphicChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cassio/polychat/TestPolymorphicChatApplication.java
@@ -0,0 +1,11 @@
+package com.cassio.polychat;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestPolymorphicChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(PolymorphicChatApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
